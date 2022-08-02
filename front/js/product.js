5566ffe878ad3b52b6d28c274ef8c70a9275be2a
Thibault Sanclemente <84449794+DevTipsy@users.noreply.github.com>
2022-08-02 11:26:11 +0200

    Update 3 script.js
    Adding addBasket

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,105 @@
-//On récupère l'id du produit dans l'url
-let params = (new URL(document.location)).searchParams;
-let _id = params.get('_id');
-let kanapID = parseInt(params.get('_id'));
-console.log(_id);
+//On récupére l'id via les paramètres de l'url
+const id = new URL(window.location.href).searchParams.get("_id");
 
+//On appelle la fonction (asynchrone) des détails d'un canapé
+InfoProduct();
+
+//On récupère l'article et affiche ses données grâce a l'id
+async function InfoProduct() {
+     await fetch("http://localhost:3000/api/products/" +id)
+    .then((response) => response.json())    
+    .then(item => {
+        //Récupération des sélecteurs pour les futures modifications
+        let title = document.getElementById("title");
+        let price = document.getElementById("price");
+        let description = document.getElementById("description");
+        let colorsP = document.getElementById("colors");
+        //On créé la balise img dans la classe item__img
+        let imgProduct = document.querySelector(".item__img");
+        let img = document.createElement("img");
+        imgProduct.appendChild(img);
+        //On définit la source et alt de l'img
+        img.setAttribute("src", item.imageUrl);
+        img.setAttribute("alt", item.altTxt);
+        //On insère les infos produit    
+        title.innerHTML = item.name;
+        price.innerHTML = item.price;
+        description.innerHTML = item.description;
+        document.title = item.name;
+        //On créé la boucle pour le choix de la couleur
+        for (let i =0; i < item.colors.length; i++) {
+        //Création de l'élément "option"
+            let color = document.createElement("option");
+            //On attribue un paramètre "value"
+            color.setAttribute("value", item.colors[i]);
+            //On insère la couleur dans le html
+            color.innerHTML = item.colors[i];
+            colorsP.appendChild(color);
+        }  
+    }) ;          
+}
+
+//Ajouter des produits au panier grâce au localstorage
+/*****************************************************/
+    function saveBasket (basket) {
+        localStorage.setItem("basket", JSON.stringify(basket));
+    }
+    //Récupérer les éléments du panier, si il n'y en a pas, retourner un tableau vide
+    function getBasket() {
+        let basket = localStorage.getItem("basket");
+        if (basket == null) {
+            return [];
+        } else {
+            return JSON.parse (basket);
+        }
+    }
+    //Ajouter des produits au panier
+    var button = document.getElementById("addToCart")
+    button.addEventListener("click",function addBasket (product) {
+        let basket = getBasket();
+        let foundProduct = basket.find(p => p.id == product.id);
+        if (foundProduct != undefined) {
+            foundProduct.quantity++;
+        } else {
+            product.quantity = 1;
+            basket.push(product); 
+    }
+    saveBasket (basket)
+    });
+
+    //Supprimer un élément du panier
+    function removefromBasket (product) {
+        let basket = getBasket();
+        basket = basket.filter(p => p.id != product.id);
+        saveBasket(basket);
+    }
+    //Modifier la quantité (si la quantité<0 alors l'article est supprimé)
+    function changeQuantity(product, quantity) {
+        let basket = getBasket();
+        let foundProduct = basket.find(p => p.id == product.id);
+        if (foundProduct != undefined) {
+            foundProduct.quantity += quantity;
+            if (foundProduct.quantity <= 0) {
+                removeFromBasket(foundProduct);
+            } else {
+                saveBasket (basket) ;
+            }
+    }
+}
+//Afficher le nombre de produits
+function getNumberProduct() {
+    let basket = getBasket();
+    let number = 0;
+    for (let product of basket) {
+        number +-product.quantity;
+    }
+    //Obtenir le prix total du panier
+    function getTotalPrice() {
+        let basket = getBasket();
+        let total = 0;
+        for (let product of basket) {
+            total += product.price * product.quantity;
+        }
+        return(total)
+    }
+}
--- front/js/script.js
+++ front/js/script.js
@@ -1,33 +1,34 @@
 /*Connexion à l'api*/
 /*********************/
 fetch("http://localhost:3000/api/products")
-/*On transforme la réponse en objet JS interprétable par le navigateur*/
+  //On transforme la réponse en objet JS interprétable par le navigateur
   .then((reponse) => reponse.json())
-/*On nomme le résultat promise*/
+  //On nomme le résultat promise
   .then((promise) => {
-/*On demande de récupérer les données sous forme de tableau dans la console*/
+    //On demande de récupérer les données sous forme de tableau dans la console
     console.table(promise);
-/*On demande d'afficher les produits de la boucle for qui suit*/
+    //On demande d'afficher les produits de la boucle for qui suit
     afficherKanap(promise);
   })
-/*Si il ya une erreur :*/
+  //Si il ya une erreur:
   .catch((err) => {
-/*On récupère l'élément class="titles" et on le remplace par "Erreur..."*/
+    //On récupère l'élément class="titles" et on le remplace par "Erreur..."
     document.querySelector(".titles").innerHTML = "<h1>Erreur, produits indisponibles</h1>";
-/*On affiche l'erreur dans la console*/
+    //On affiche l'erreur dans la console
     console.log("Erreur");
   });
+
 /*Création des cartes produits*/
 /******************************/
-/*On utilise le même nom que la promise pour la fonction en précisant la page concernée*/
+//On utilise le même nom que la promise pour la fonction en précisant la page concernée
 function afficherKanap(index) {
-/*On déclare une variable en utilisant l'ID items */
+  //On déclare une variable en utilisant l'ID items 
   let produits = document.querySelector("#items");
-/*On créé la boucle for avec la balise article de l'index*/
+  //On créé la boucle for avec la balise article de l'index
   for (let article of index) {
-/*On insère via innerHTML les caractéristiques des produits.
-On utilise un lien propre à chaque produit grâce à lien+_id=${produit._id}
-On utilise $ pour l'image, alt, name et description afin de récupérer les données propre à chaque produit*/
+    /*On insère via innerHTML les caractéristiques des produits.
+    On utilise un lien propre à chaque produit grâce à lien+_id=${produit._id}
+    On utilise $ pour l'image, alt, name et description afin de récupérer les données propre à chaque produit*/
     produits.innerHTML += `<a href="./product.html?_id=${article._id}">
     <article>
       <img src="${article.imageUrl}" alt="${article.altTxt}">
